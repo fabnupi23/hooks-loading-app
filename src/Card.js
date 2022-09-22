8a214ca0b2d7c210328dba76b4ca78a2e86edc3c
Fabian_Nuñez <31360808+fabnupi23@users.noreply.github.com>
2022-09-21 23:53:02 -0500

    Estructura  ejercicio loading  con hooks

---
+++ src/Card.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <div>
+        <h1>
+            Soy Card
+        </h1>
+    </div>
+  )
+}
+
+export { Card };
---
+++ src/LoadingApp.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Card } from './Card';
+
+function LoadingApp() {
+  return (
+    <div>
+        <Card/>
+        <h1>
+            Soy LoadingApp
+        </h1>
+    </div>
+  )
+}
+
+export { LoadingApp };
